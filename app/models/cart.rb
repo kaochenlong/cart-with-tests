c0022f8dd3526c62aebc1a684f646fab2af6e3aa
Eddie Kao <eddie@digik.com.tw>
2016-07-07 12:04:50 +0800

    add cart_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,29 @@
+class CartItem
+end
+
 class Cart
+  attr_reader :items
+
   def initialize
     @items = []
   end
 
-  def add_item(product)
-    @items << product
+  def add_item(product_id)
+    # 找： item = 找 item
+    item = @items.find { |x| x.product_id == product_id }
+
+    # if 有一樣的
+    if item
+      item.increment
+    else
+      @items << CartItem.new(product_id)
+    end
+
+    #   item.quantity += 1
+    # else
+    #   @items < new item
+
+    #@items << product
   end
 
   def empty?
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -9,5 +9,19 @@ RSpec.describe Cart, type: :model do
       cart.add_item(1)
       expect(cart.empty?).to be false
     end
+
+    it "加一樣的商品不會增加 item 數" do
+      cart = Cart.new
+      5.times do
+        cart.add_item(1)
+      end
+      3.times do
+        cart.add_item(2)
+      end
+
+      expect(cart.items.count).to be 2
+      expect(cart.items.first.quantity).to be 5
+      expect(cart.items.last.quantity).to be 3
+    end
   end
 end
